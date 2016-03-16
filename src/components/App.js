4e3f8be50286f11fb7692c799d1972cb08752d02
Joey Di Nardo <yejodido@gmail.com>
2016-03-15 20:16:49 -0400

    basic framking

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,12 @@ import Header from './Header';
 import Body from './Body';
 import Contact from './Contact';
 import Footer from './Footer';
+import styles from './styles/App.scss';
 
 export default class App extends Component {
     render() {
         return (
-            <div>
+            <div className={styles.App}>
                 <Header />
                 <Body />
                 <Contact />
