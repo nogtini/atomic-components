88fa7f67747b619ae60dc34a728897064fe935be
Joey Di Nardo <yejodido@gmail.com>
2016-03-16 20:19:32 -0400

    adding services graphics and styles

--- src/components/body.js
+++ src/components/body.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import globalStyles from '../assets/styles/globals.scss';
 import localStyles from './styles/Body.scss';
 
+//assets
+import backend from '../assets/images/backend.svg';
+import frontend from '../assets/images/frontend.svg';
+import design from '../assets/images/design.svg';
+
 const styles = Object.assign({}, localStyles, globalStyles);
+
 export default class Body extends Component {
     render() {
         return (
@@ -10,8 +16,18 @@ export default class Body extends Component {
                 <div className={styles.flexLeft} />
                 <div className={`${styles.flexMiddle} ${styles.main}`}>
                     <h1>Services</h1>
-                    <div classNames={`${styles.backfrontdesign}`}></div>
-                    <div classNames={`${styles.trails}`}></div>
+                    <div className={`${styles.backfrontdesign}`}>
+                        <div className={`${styles.backend}`}>
+                            <img className={styles.backendimage} src={backend} />
+                        </div>
+                        <div className={`${styles.frontend}`}>
+                            <img className={styles.frontendimage} src={frontend} />
+                        </div>
+                        <div className={`${styles.design}`}>
+                            <img className={styles.designimage} src={design} />
+                        </div>
+                    </div>
+                    <div className={`${styles.trails}`}></div>
                 </div>
                 <div className={styles.flexLeft} />
             </div>
