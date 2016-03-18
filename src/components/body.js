053b19b56775163127f5a58162740413d43c0a2e
Joey Di Nardo <yejodido@gmail.com>
2016-03-17 20:23:01 -0400

    more reasonable margins

--- src/components/body.js
+++ src/components/body.js
@@ -20,17 +20,17 @@ export default class Body extends Component {
                         <div className={`${styles.backend}`}>
                             <img className={styles.serviceImage} src={backend} />
                             <h2>Node JS</h2>
-                            <div className={styles.serviceBody}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut mi ut augue tincidunt facilisis a vel felis. Duis aliquet diam vel quam fermentum cursus.</div>
+                            <div className={styles.serviceBody}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut mi ut augue tincidunt facilisis a vel felis.</div>
                         </div>
                         <div className={`${styles.frontend}`}>
                             <img className={styles.serviceImage} src={frontend} />
                             <h2>Front-End</h2>
-                            <div className={styles.serviceBody}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut mi ut augue tincidunt facilisis a vel felis. Duis aliquet diam vel quam fermentum cursus.</div>
+                            <div className={styles.serviceBody}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut mi ut augue tincidunt facilisis a vel felis.</div>
                         </div>
                         <div className={`${styles.design}`}>
                             <img className={styles.serviceImage} src={design} />
                             <h2>Design</h2>
-                            <div className={styles.serviceBody}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut mi ut augue tincidunt facilisis a vel felis. Duis aliquet diam vel quam fermentum cursus.</div>
+                            <div className={styles.serviceBody}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut mi ut augue tincidunt facilisis a vel felis.</div>
                         </div>
                     </div>
                     <div className={`${styles.trails}`}></div>
