528c253811d6fb8e31bbd8840b0c1e580c422003
Joey Di Nardo <yejodido@gmail.com>
2016-03-23 21:51:01 -0400

    beginnings of input adding and styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,6 +4,7 @@ import localStyles from './styles/Contact.scss';
 
 //assets
 import skyline from '../assets/images/skyline.svg';
+import tower from '../assets/images/tower.svg';
 import team from '../assets/images/teamblue.png';
 
 const styles = Object.assign({}, localStyles, globalStyles);
@@ -13,6 +14,13 @@ export default class Contact extends Component {
             <div className={styles.Contact}>
                 <div className={styles.flexLeft} />
                 <div className={`${styles.flexMiddle}`}>
+                    <div className={`${styles.contactTower}`}>
+                        <div className={`${styles.form}`}>
+                            <input></input>
+                            <input></input>
+                        </div>
+                        <img src={tower} className={`${styles.tower}`}/>
+                    </div>
                     <img src={team} className={`${styles.teamimage}`}/>
                 </div>
                 <div className={styles.flexLeft} />
