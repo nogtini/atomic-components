196b00dc69c9214d36070650b10a400bcb70e68f
Joey Di Nardo <yejodido@gmail.com>
2016-03-28 19:57:16 -0400

    added font awesome and nonfinal styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import globalStyles from '../assets/styles/globals.scss';
 import localStyles from './styles/Footer.scss';
 
+//assets
+import logooutline from '../assets/images/logooutline.svg';
+
 const styles = Object.assign({}, localStyles, globalStyles);
 export default class Footer extends Component {
     render() {
         return (
             <div className={styles.Footer}>
                 <div className={styles.flexLeft} />
-                <div className={`${styles.flexMiddle}`}></div>
+                <div className={`${styles.flexMiddle} main`}>
+                    <div className={`${styles.icons}`}>
+                        <a className="fa fa-facebook social" href="#"></a>
+                        <a className="fa fa-twitter social" href="#"></a>
+                        <img src={logooutline} className={`${styles.logooutline}`}/>
+                        <a className="fa fa-dribbble" href="#"></a>
+                        <a className="fa fa-linkedin" href="#"></a>
+                    </div>
+                </div>
                 <div className={styles.flexLeft} />
             </div>
         );
