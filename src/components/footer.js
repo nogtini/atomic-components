6b1a1e7a154695d1ea35f69daf0fece3815d27c7
Travis <me@traviswebb.com>
2016-04-11 04:52:55 -0400

    set social media hrefs

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,11 +13,11 @@ export default class Footer extends Component {
                 <div className={styles.flexLeft} />
                 <div className={`${styles.flexMiddle} main`}>
                     <div className={`${styles.icons}`}>
-                        <div className={`${styles.social} fa fa-facebook`} href="#"></div>
-                        <div className={`${styles.social} fa fa-twitter`} href="#"></div>
+                        <a className={`${styles.social} fa fa-facebook`} href="//www.facebook.com/langainc" target="_blank"></a>
+                        <a className={`${styles.social} fa fa-twitter`} href="//twitter.com/langateam" target="_blank"></a>
                         <img src={logooutline} className={`${styles.logooutline}`}/>
-                        <div className={`${styles.social} fa fa-dribbble`} href="#"></div>
-                        <div className={`${styles.social} fa fa-linkedin`} href="#"></div>
+                        <a className={`${styles.social} fa fa-dribbble`} href="//dribbble.com/langateam" target="_blank"></a>
+                        <a className={`${styles.social} fa fa-linkedin`} href="//www.linkedin.com/company/langa-inc-" target="_blank"></a>
                     </div>
                 </div>
                 <div className={styles.flexLeft} />
