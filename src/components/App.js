c451ae3e163743377d1a1cdd149cffa3fd5f793c
Travis <me@traviswebb.com>
2016-04-14 01:02:06 -0400

    fix imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Body from './Body';
-import Contact from './Contact';
-import Footer from './Footer';
+import Header from './header';
+import Body from './body';
+import Contact from './contact';
+import Footer from './footer';
 import styles from './styles/App.scss';
 
 export default class App extends Component {
