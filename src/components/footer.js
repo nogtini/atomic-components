2d41ff73a623b4e19651164fd83dc196e9d61477
Travis J. Webb <me@traviswebb.com>
2017-02-06 22:16:20 -0500

    [fix] positioning from merge chaos

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,18 +9,12 @@ export default class Footer extends Component {
   render() {
     return (
       <div className={styles.Footer}>
-        <div className={styles.flexLeft} />
-        <div className={`${styles.flexMiddle} main`}>
-          <div className={`${styles.icons}`}>
-            <a title="Langa on Facebook" className={`${styles.social} fa fa-facebook`} href="//www.facebook.com/langainc" target="_blank"></a>
-            <a title="Langa on Twitter" className={`${styles.social} fa fa-twitter`} href="//twitter.com/langateam" target="_blank"></a>
-            <img src={logooutline} className={`${styles.logooutline}`}/>
-            <a title="Langa on Github" className={`${styles.social} fa fa-github-alt`} href="//github.com/langateam" target="_blank"></a>
-            <a title="Langa on Dribbble" className={`${styles.social} fa fa-dribbble`} href="//dribbble.com/langateam" target="_blank"></a>
-            <a title="Langa on Linkedin" className={`${styles.social} fa fa-linkedin`} href="//www.linkedin.com/company/langa-inc-" target="_blank"></a>
-          </div>
+        <div className={`${styles.Container} ${styles.icons}`}>
+          <a title="Langa on Facebook" className={`${styles.social} fa fa-facebook`} href="//www.facebook.com/langainc" target="_blank"></a>
+          <a title="Langa on Twitter" className={`${styles.social} fa fa-twitter`} href="//twitter.com/langateam" target="_blank"></a>
+          <a title="Langa on Github" className={`${styles.social} fa fa-github-alt`} href="//github.com/langateam" target="_blank"></a>
+          <a title="Langa on Linkedin" className={`${styles.social} fa fa-linkedin`} href="//www.linkedin.com/company/langa-inc-" target="_blank"></a>
         </div>
-        <div className={styles.flexLeft} />
       </div>
     )
   }
